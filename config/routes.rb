326c205ee11182f575fa7fc4dc5b7b76d286e5e9
Max Jilu Wang <wangjilu23@gmail.com>
2022-02-28 16:29:58 +1100

    hell yeah

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  devise_for :users
+  root to: 'lessons#index'
+  resources :lessons, only: [:index, :show, :edit, :new, :create, :destroy ] do
+    resources :sessions, only: [:show, :new, :create, :destroy]
+  end
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
